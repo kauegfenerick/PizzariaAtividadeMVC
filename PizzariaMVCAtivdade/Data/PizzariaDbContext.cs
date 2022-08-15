4fe4f0d9745b5207c827b760d1693526182e11e6
KauePessoal <kauegomesfenerick@gmail.com>
2022-08-15 13:13:59 -0300

    Configuração inicial do DB

--- PizzariaMVCAtivdade/Data/PizzariaDbContext.cs
+++ PizzariaMVCAtivdade/Data/PizzariaDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using PizzariaMVCAtivdade.Controllers;
-using PizzariaMVCAtivdade.Controllers.Inferfaces;
 using System;
+using PizzariaMVCAtivdade.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PizzariaMVCAtivdade.Controllers.Inferfaces;
+using PizzariaMVCAtivdade.Controllers;
 
 namespace PizzariaMVCAtivdade
 {
